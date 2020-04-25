c198ac63472d5a672e4702ee4c9952274a134cae
saisaira <saitarakdevarapalli@gmail.com>
2020-04-26 00:17:38 +0530

    Updates

---
+++ precache-manifest.fe4f5e6928a96f311b0574b46b7a4083.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a37e9448e0f80d673ce77057f8a81513",
+    "url": "/github.io/profule/index.html"
+  },
+  {
+    "revision": "6021e4651e9ab3424ce9",
+    "url": "/github.io/profule/static/css/main.c0d663da.chunk.css"
+  },
+  {
+    "revision": "076d0ecb26eb6044d493",
+    "url": "/github.io/profule/static/js/2.97ac240a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github.io/profule/static/js/2.97ac240a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6021e4651e9ab3424ce9",
+    "url": "/github.io/profule/static/js/main.c0b13c8e.chunk.js"
+  },
+  {
+    "revision": "daaa8f487afd24cd461a",
+    "url": "/github.io/profule/static/js/runtime-main.e27e10bd.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/github.io/profule/precache-manifest.fe4f5e6928a96f311b0574b46b7a4083.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github.io/profule/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
